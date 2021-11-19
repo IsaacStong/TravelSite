27fe844b19ffbc35914d5ce46c6c89b1f1d6b49f
IsaacStong <isaacstong90@gmail.com>
2021-11-19 11:39:58 -0800

    Models and Bug Fix
    Fixed bug where static wasn't loading. Also starting implementing models into my views and loaded static into news.html.
    TODO: Remainder of HTML pages besides index and news.

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Destinations
+
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destinations()
+    dest1.name = "Greece"
+    dest1.description = "Good place to visit."
+    dest1.price = "1,764"
+    dest1.img = "destination_6.jpg"
+    return render(request, "index.html", {'dest1': dest1})
+
+
+def news(request):
+    return render(request, "news.html")
