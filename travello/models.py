58a0155c94dc93a50ad72842a81112871f8cddd7
IsaacStong <isaacstong90@gmail.com>
2021-11-22 09:48:37 -0800

    Added Admin Capabilities
    Added Destinations to the admin page with sqlite backend. Destinations can now be created, updated, and deleted from admin page.

--- travello/models.py
+++ travello/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 # Create your models here.
-class Destinations:
+'''class Destinations:
     id: int
     name: str
     img: str
     description: str
     price: str
     offer: bool
+'''
+
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pcs')
+    desc = models.TextField()
+    price = models.CharField(max_length=10)
+    offer = models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -1,45 +1,45 @@
 from django.shortcuts import render
-from .models import Destinations
+from .models import Destination
 
 
 # Create your views here.
 def index(request):
-    dest1 = Destinations()
+    dest1 = Destination()
     dest1.name = "Greece"
     dest1.description = "Good place to visit."
     dest1.price = "1,764"
     dest1.img = "destination_6.jpg"
     dest1.offer = True
 
-    dest2 = Destinations()
+    dest2 = Destination()
     dest2.name = "Great Britain"
     dest2.description = "England is pretty cool."
     dest2.price = "1,335"
     dest2.img = "destination_1.jpg"
     dest2.offer = True
 
-    dest3 = Destinations()
+    dest3 = Destination()
     dest3.name = "Dublin"
     dest3.description = "Rolling countryside by a great city."
     dest3.price = "2,100"
     dest3.img = "destination_2.jpg"
     dest3.offer = False
 
-    dest4 = Destinations()
+    dest4 = Destination()
     dest4.name = "Cairo"
     dest4.description = "Not too far from the great pyramids!"
     dest4.price = "3,300"
     dest4.img = "destination_3.jpg"
     dest4.offer = False
 
-    dest5 = Destinations()
+    dest5 = Destination()
     dest5.name = "Berlin"
     dest5.description = "Pretzel and beer lovers welcome!"
     dest5.price = "2,567"
     dest5.img = "destination_4.jpg"
     dest5.offer = True
 
-    dest6 = Destinations()
+    dest6 = Destination()
     dest6.name = "Tokyo"
     dest6.description = "Amazing to visit during cherry blossom season."
     dest6.price = "4,043"
